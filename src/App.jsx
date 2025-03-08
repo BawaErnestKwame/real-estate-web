a917855d23cc395d605cb8b3a4d268213bad91c2
Bawa Ernest Kwame <bawaernest926@gmail.com>
2025-03-08 12:06:40 +0000

    contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,20 @@ import Header from './Components/Header'
 import About from './Components/About'
 import Projects from './Components/Projects'
 import Testimonials from './Components/Testimonials'
+import Contact from './Components/Contact'
+
+
+import { ToastContainer} from 'react-toastify';
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
+      <ToastContainer/>
       <Header/>
       <About/>
       <Projects/>
       <Testimonials/>
+      <Contact/>
     </div>
   )
 }
