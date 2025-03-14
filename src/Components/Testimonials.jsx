1cec339819f8a36d6fbe3515156e6871acb80204
Bawa Ernest Kwame <bawaernest926@gmail.com>
2025-03-14 22:01:08 +0000

    update

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 const Testimonials = () => {
   return (
     <motion.div className='container mx-auto py-10 lg:px-32 w-full overflow-hidden'id='Testimonials'
-    initial={{ opacity: 0, x: 200 }}  
+    initial={{ opacity: 0, x: -200 }}  
     whileInView={{ opacity: 1, x: 0 }}  
     transition={{ duration: 1 }}
     viewport={{ once: false, amount: 0.3 }} 
